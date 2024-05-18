adde82fe22184266f1a6c817540381ac125a8988
root <malikovance@gmail.com>
2024-05-18 06:34:45 +0000

    tired

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *program that prints the size of various types
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	int a;
+	long int b;
+	long long in c;
+	char d;
+	float;
+	printf("Size of a chr: %lu byte (s)/n" , (unsigned long)sizeof(d));
+
