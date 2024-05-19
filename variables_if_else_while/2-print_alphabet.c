02757cf25fdb22c91e22cf89a63157839c1f0c7b
root <malikovance@gmail.com>
2024-05-19 23:00:39 +0000

    lower case alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,3 +2,18 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - this c program prints the alphabet in lowercase,
+ * followed by a new line. 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	putchar('\n');
+	return (0);
+}
