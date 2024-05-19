6a423ea73bb9772bbd3999895b46ba4011d0a636
root <malikovance@gmail.com>
2024-05-19 17:17:46 +0000

    fi

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry
- * assinging random number to the variable n.
- * Return: Always 0 (Success)
+ *main - Entry
+ *assinging random number to the variable n.
+ *Return: Always 0 (Success)
  */
 
 int main(void)
