038d4c162ebad8bc0374169e40acff3af6e4dc00
root <malikovance@gmail.com>
2024-05-19 15:43:44 +0000

    hot fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+{
 	int a;
 	long int b;
-	long long in c;
+	long long int c;
 	char d;
-	float;
-	printf("Size of a chr: %lu byte (s)/n" , (unsigned long)sizeof(d));
-
+	float f;
+	printf("Size of a chr: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsinged long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsinged long)sizeof(c));
+	printf9("Size of a float: %lu byet(s)\n". (singed long )sizeof(f));
+	return (0);
+	}
