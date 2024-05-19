c7cd932df59a3680c5ebbc4a07fe148879dc3c16
root <malikovance@gmail.com>
2024-05-19 22:04:39 +0000

    doing a lot of small fixes

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main
- * Shortdescription 
- * Return: Always 0 (Success)
+ * i must put a string with last digit of followed by n
+ * Return: Always 0
  */
-
 int main(void)
 {
 	int n;
