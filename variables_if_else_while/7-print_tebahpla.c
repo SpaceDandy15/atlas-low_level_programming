36a860894f0a36b930511cabf97866bbcd4cc508
root <malikovance@gmail.com>
2024-05-20 03:34:01 +0000

    reverse reverse

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - prgram that prints lowercase alphabet in reverse putchar only
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
