e18cfd20282fea60bb66f270fd6d81931eb6d742
root <malikovance@gmail.com>
2024-05-20 04:10:44 +0000

    fix

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,18 +7,21 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int d;
+
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 		if (d != '9')
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
+
 	return (0);
 }
