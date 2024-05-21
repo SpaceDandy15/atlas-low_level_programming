702d7088d7fab5672007889f441600b928c7db43
root <malikovance@gmail.com>
2024-05-21 04:59:28 +0000

    figure it out later

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -10,10 +10,10 @@ int main(void)
 	int r;
 
 	r = _isalpha('H');
-	_putch(r+'0');
+	_putchar(r+'0');
 	r = _isalpha('o');
 	_putchar(r+ '0');
-	r = -isalpha(108);
+	r = _isalpha(108);
 	_putchar(r + '0');
 	r = _isalpha(';');
 	_putchar(r + '0');
