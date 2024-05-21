cfc286cb3d251049dd4ec3d6cde75b8ae3d7c1f9
root <malikovance@gmail>
2024-05-21 22:19:55 +0000

    function prints sign of the number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,7 +2,29 @@
 #include "main.h"
 
 /**
- * main -
- *
- * Return: 0
+ * print_sign - prints the sign of the number
+ * @n: input number
+ * Return: 1 if number is positivie, o if number is 0, or 
+ * -1 if number is negative
  */
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if ( n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value =-1;
+		_putchar('-');
+	}
+	return (value);
+}
