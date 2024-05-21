074a6d3f72ae18f366f1b0980394b2721f609330
root <malikovance@gmail>
2024-05-21 22:22:20 +0000

    fix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
 /**
  * print_sign - prints the sign of the number
  * @n: input number
- * Return: 1 if number is positivie, o if number is 0, or 
+ * Return: 1 if number is positive, o if number is 0, or
  * -1 if number is negative
  */
 int print_sign(int n)
@@ -16,14 +16,14 @@ int print_sign(int n)
 		value = 1;
 		_putchar('+');
 	}
-	else if ( n == 0)
+	else if (n == 0)
 	{
 		value = 0;
 		_putchar('0');
 	}
 	else
 	{
-		value =-1;
+		value = -1;
 		_putchar('-');
 	}
 	return (value);
