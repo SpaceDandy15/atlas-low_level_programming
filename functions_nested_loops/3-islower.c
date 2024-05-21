ab831829ba5f51f35f2ab64ec3be3a174cfb7429
root <malikovance@gmail>
2024-05-21 21:42:22 +0000

    fix for betty

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - check
- *
- * alphabet in lowercase 10 times
+ *3-islower.c - checks for lower case character
  *
  * Return: 1 if is lowercase or 0 if is uppercase
  */
