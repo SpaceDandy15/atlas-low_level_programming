9e4e464ea10f928fb4446c1661d5d0ba7af527ce
root <malikovance@gmail>
2024-05-21 22:41:09 +0000

    fucntion that prints every minute in a day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,7 +2,23 @@
 #include "main.h"
 
 /**
- * main -
+ * jack_bauer - function prints every minute in one day
  *
- * Return: 0
+ * Return: Nothing
  */
+void jack_bauer(void)
+{
+	int H, M;
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
