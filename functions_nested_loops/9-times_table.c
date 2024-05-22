dbcdc519a9382714fae371cb4d126661efe0c39a
root <malikovance@gmail>
2024-05-22 00:29:55 +0000

    fix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -9,15 +9,15 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for( i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j =0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
 
 			if ((n / 10) == 0)
 			{
-				if (j !=0)
+				if (j != 0)
 					_putchar(' ');
 				_putchar(n + '0');
 
@@ -28,7 +28,7 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((n /10) + '0');
+				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
 				if (j == 9)
 					continue;
