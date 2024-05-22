517080f2837e096486a47e0add77973571c982ac
root <malikovance@gmail>
2024-05-22 00:33:19 +0000

    fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d",n);
+			printf("%d", n);
 
 			if (n == 98)
 				continue;
@@ -30,7 +30,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf("\n");
+			printf(", ");
 		}
 		printf("\n");
 	}
