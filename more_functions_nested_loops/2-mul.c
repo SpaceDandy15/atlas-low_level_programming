5aa9d0b7f6c80030108aa5ce28ff7107919e2ce2
root <malikovance@gmail.com>
2024-05-26 15:43:09 +0000

    mulitplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 int
+ * @a: int to be multiplied to b
+ * @b: int to be mulitplied to a
+ * Return: the result o the operation
+ */
+int multi (int a , int b)
+{
+	int c;
+	c = a * b;
+
+	return (c);
+}
