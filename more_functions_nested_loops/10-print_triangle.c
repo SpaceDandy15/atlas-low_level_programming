d19526432edc4fc0be793f0f0e6df2282141f4a1
root <malikovance@gmail.com>
2024-05-26 20:27:14 +0000

    fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,21 +7,21 @@
 
 void print_triangle(int size)
 {
-	if (size <=0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
 		int i, m;
 
-		for ( i = 1; i <= size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (m = i; m < size; m++)
 			{
 				_putchar(' ');
 			}
-			
-			for (m = 1; m <= i; m ++)
+
+			for (m = 1; m <= i; m++)
 			{
 				_putchar('#');
 			}
