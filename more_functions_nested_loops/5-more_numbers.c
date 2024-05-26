8037cbc7c1d7bc5809d13f825525aa61e42bf349
root <malikovance@gmail.com>
2024-05-26 17:33:28 +0000

    fix

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,10 +8,11 @@ void more_numbers(void)
 {
 	int i, m;
 	for (i = 0; i < 10; i++)
+
 	{
 		for (m = 0; m < 15; m++)
 		{
-			if( m >= 10)
+			if (m >= 10)
 				_putchar(m / 10 + '0');
 			_putchar(m % 10 + '0');
 		}
