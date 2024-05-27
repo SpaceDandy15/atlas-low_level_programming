8f6f89b4bb8c5eed431fac42d4e6ad2b1dc80533
root <malikovance@gmail.com>
2024-05-27 23:48:18 +0000

    pritns a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	char *end = s;
+	
+	while (*end) 
+	{
+		++end;
+	}
+	--end;
+	while (end >= s) {
+		_putchar(*--end);
+	}
+	_putchar('\n');
+}
