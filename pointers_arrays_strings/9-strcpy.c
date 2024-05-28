94f30c6b1f2f277a47a428ecca91948dafbc1732
root <malikovance@gmail.com>
2024-05-28 03:39:51 +0000

    fix

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,7 +9,7 @@
  *
  * Return: the pointer to dest
  */
-char *_strcpy(char * dest, char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
@@ -19,8 +19,7 @@ char *_strcpy(char * dest, char *src)
 	{
 		len++;
 	}
-	
-	for ( i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
