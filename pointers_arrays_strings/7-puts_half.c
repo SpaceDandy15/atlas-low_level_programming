4c684341d06f8bee495dd2396284c3c1eb544072
root <malikovance@gmail.com>
2024-05-28 03:47:40 +0000

    big fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,15 +6,27 @@
  */
 void puts_half(char *str)
 {
-	int len, mid, i;
-	
-	for (len = 0; str[len] != '\0'; len++);
-	mid = len / 2;
-	if (len % 2 != 0)
-		mid++;
-	
-	for (i = mid; i < len; i++)
-		_putchar(str[i]);
+	int len , n , i;
 
-	_putchar('\n');
+	len =0;
+	
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for ( i = len / 2; str [i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len -1; n++)
+		{
+			_putchar(str[n+1]);
+		}
+	}
 }
