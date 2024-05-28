efe691418f2c322a70dbd44d04ec33618e620443
root <malikovance@gmail.com>
2024-05-28 04:45:50 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -17,19 +17,18 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;	
 	while (s[len] != '\0')
-			len++;
+		len++;
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
 				digit = -digit;
-			n = n * 10 + digit ;
-			f = 1 ;
+			n = n * 10 + digit;
+			f = 1;
 			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
