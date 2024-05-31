26838ab8abc6fbc03c9c506d32ffc28e15fbc38d
Malik Vance <malik@atlassian.com>
2024-05-31 08:46:06 -0500

    edit

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,14 @@
 char *_strcat(char *dest, char *src)
 {
     int i, m;
-
-    // find the end of the dest string
+    
     for (i = 0; dest[i]!= '\0'; i++);
 
-    // Append src to dest
     for (m = 0; src[m]!= '\0'; m++, i++)
     {
         dest[i] = src[m];
     }
 
-    // Null terminate the result
     dest[i] = '\0';
 
     return dest;
