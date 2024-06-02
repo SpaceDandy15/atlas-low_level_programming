42e0efb4f2cc19b6f92b741d4354173032cf6347
Malik Vance <malik@atlassian.com>
2024-06-02 13:49:09 -0500

    fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * @s1: first string to compare
- * @s2: secpmd stromg to compare
+ * @s2: second string to compare
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
  * more than 0 if s1 is treated than s2
  */
