50cf8a3976cf5f1a9741840f6ec7187a6264cd4b
Malik Vance <malik@atlassian.com>
2024-06-02 10:16:14 -0500

    fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,16 +2,19 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest: string to append to 
+ * @dest: string to append to
  * @src: string to append
  * Return: pointer to the string
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, m;
-    for (i = 0; dest[i]!= '\0'; i++);
-    for (m = 0; src[m]!= '\0'; m++, i++)
-    {dest[i] = src[m];
+int i, m;
+    
+for (i = 0; dest[i] != '\0'; i++);
+
+for (m = 0; src[m]!= '\0'; m++, i++)
+    {
+    dest[i] = src[m];
     } 
     dest[i] = '\0';return dest;
 }
