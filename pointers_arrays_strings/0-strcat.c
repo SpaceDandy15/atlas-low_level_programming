c3f605e60312f9bd28b67eaeee0e54b62341f212
Malik Vance <malik@atlassian.com>
2024-06-02 10:12:48 -0500

    detect indetation needed to be turned off.

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,9 @@
 char *_strcat(char *dest, char *src)
 {
     int i, m;
-    
     for (i = 0; dest[i]!= '\0'; i++);
-
     for (m = 0; src[m]!= '\0'; m++, i++)
-    {
-        dest[i] = src[m];
-    }
-
-    dest[i] = '\0';
-
-    return dest;
+    {dest[i] = src[m];
+    } 
+    dest[i] = '\0';return dest;
 }
