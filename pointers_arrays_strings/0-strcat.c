0f3008cccb18aafc86af83f88ebc2c145329db35
Malik Vance <malik@atlassian.com>
2024-06-02 10:56:10 -0500

    i think i finally fixed my spacing settings on vs code

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, m;
+	int i, m;
 
-    m = 0;
-    i = 0;
-    while (dest[i] != '\0')
-        i++;
+	i = 0;
+	m = 0;
 
-    while (src[m] != '\0')
-    {
-            dest[i] = src[m];
-            m++;
-            i++;
-    }
-    dest[i] = '\0';
+	while (dest[i] != '\0')
+			i++;
 
-    return (dest);
+	while (src[m] != '\0')
+	{
+		dest[i] = src[m];
+		m++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
