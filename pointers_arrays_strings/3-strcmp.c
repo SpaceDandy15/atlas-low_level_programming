b7c4b288e7cf00338f73cb41b20d6d2905b52f54
Malik Vance <malik@atlassian.com>
2024-06-02 19:25:21 -0500

    fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * _strcmp - compares two strings
  * @s1: first string to compare
  * @s2: second string to compare
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
