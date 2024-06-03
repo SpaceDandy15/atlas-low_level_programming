bf70c37d2988eb4cecff4b873bb212e646e17ca3
Malik Vance <malik@atlassian.com>
2024-06-03 08:48:37 -0500

    whitespace line 7

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * @dest: destination memory area
  * @src: memeory area to copy from
  * @n: number of bytes to copy
- * 
+ *
  * Return: pointer to dest
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
