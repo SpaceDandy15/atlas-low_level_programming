316e7d3562c72e6e2bcf72f1fd91d1fd331b8770
Malik Vance <malik@atlassian.com>
2024-06-03 20:56:26 -0500

    i always put char instead oc chr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,11 +5,11 @@
  * *_strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
+ * 
  * Return: a pointer to the first occurence of the
- * character c in the string s, or Null if
- * the character is not found
+ * character c in the string s, or Null if the character is not found
  */
-char *_strchar(char *s, char c)
+char *_strchr(char *s, char c)
 {
 	int a;
 
