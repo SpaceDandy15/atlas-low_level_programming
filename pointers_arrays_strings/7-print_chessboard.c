013f541474b471168119d6d8c7e9c38d1c398d28
Malik Vance <malik@atlassian.com>
2024-06-03 22:02:16 -0500

    fixes

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,9 +9,9 @@ void print_chessboard(char (*a)[8])
 {
 	int i, m;
 
-	for(i = 0; i < 8; i++)
+		for (i = 0; i < 8; i++)
 	{
-			for(m = 0; m < 8; m++)
+			for (m = 0; m < 8; m++)
 			{
 				_putchar(a[i][m]);
 			}
