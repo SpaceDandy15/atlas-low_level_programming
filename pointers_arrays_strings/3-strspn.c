2b48866c71d0bebb5eab4d703d60cd0acbf83683
Malik Vance <malik@atlassian.com>
2024-06-03 21:21:01 -0500

    fixes

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -4,19 +4,19 @@
  * *_strspn - gets the lenght of a prefix substring
  * @s: string to evaluate
  * @accept: string containing the list of characters to match ins
- * 
+ *
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i , m, v, flag;
-	
+	int i, m, v, flag;
+
 	v = 0;
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		flag = 0;
-		for (m = 0; accept [m] != '\0'; m++)
+			for (m = 0; accept[m] != '\0'; m++)
 		{
 				if (s[i] == accept[m])
 				{
