73aaa1eac63e9abe43427744397b6271a0ccb11d
Malik Vance <malik@atlassian.com>
2024-06-03 20:57:09 -0500

    white space

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * *_strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
- * 
+ *
  * Return: a pointer to the first occurence of the
  * character c in the string s, or Null if the character is not found
  */
