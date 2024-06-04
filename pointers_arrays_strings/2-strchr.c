aac027efc88b02e9c04a74e5551068ce7cf4f86a
Malik Vance <malik@atlassian.com>
2024-06-03 20:43:25 -0500

    didnt like that code

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_strchr - locates a character in a string
- * @param s1 first string to compare.
- * @param s2 second string to compare.
- * Return: Less than 0 if s1 < s2, 0 if s1 == s2,
- * more than 0 if s1 > s2
-*/
-char *_strchr(char *s, char c)
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: less than 0 if s1 is less than s2, 
+ * 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
-		if(*s1 == '0')
+		if (*s1 == '\0')
 		{
-			return 0;
+			return (0);
 		}
 		s1++;
 		s2++;
