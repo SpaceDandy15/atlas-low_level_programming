d2d9082dacd4927abdf2a6831bc72ccd88b37c62
Malik Vance <malik@atlassian.com>
2024-06-03 21:54:59 -0500

    function that prints the chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two dimension array to print
+*/
+
+void print_chessboard(char (*a) [8])
+{
+	int i, m;
+
+	for(i = 0; i < 0; i++)
+	{
+			for(m = 0; m < 8; m++)
+			{
+				_putchar(a[i][m]);
+			}
+			_putchar('\n');
+	}
+}
