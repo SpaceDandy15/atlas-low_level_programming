821cab292cf3a4f45ae57233416a78a4436760b8
Malik Vance <malik@atlassian.com>
2024-06-11 20:41:25 -0500

    fixes

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 	f = 0;
 	digit = 0;
 
-	while (s[len] != '\0')
+		while (s[len] != '\0')
 			len++;
 
 	while (i < len && f == 0)
@@ -29,11 +29,11 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
-			if (d % 2)
+		if (d % 2)
 					digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[i + 1] < '0' || s[i +1] > '9')
+		if (s[i + 1] < '0' || s[i + 1] > '9')
 					break;
 			f = 0;
 		}
@@ -42,7 +42,7 @@ int _atoi(char *s)
 
 	if (f == 0)
 			return (0);
-	
+
 	return (n);
 }
 
