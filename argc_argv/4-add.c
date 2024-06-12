48023ead854801e1e7512eb02525a0dedaf746d9
Malik Vance <malik@atlassian.com>
2024-06-11 22:46:05 -0500

    program that adds positive numbers.

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -53,3 +53,33 @@ int _atoi(char *s)
  *
  * Return: 0 (Success), or 1 (Success)  
 */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+
+	for ( i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (arg [i][j] < '0')
+			{
+				puts("Error");
+				return (1); 
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
