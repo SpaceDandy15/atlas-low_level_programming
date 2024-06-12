b4cc575735fb5c8fd868d54139ed80fcf2e58149
Malik Vance <malik@atlassian.com>
2024-06-11 23:24:47 -0500

    good ol betty

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -21,11 +21,11 @@ int _atoi(char *s)
 	while (s[len] != '\0')
 		len++;
 
-	while (i < len && f == 0)
+		while (i < len && f == 0)
 	{
 			if (s[i] == '-')
 				++d;
-			
+
 			if (s[i] >= '0' && s[i] <= '9')
 			{
 				digit = s[i] - '0';
@@ -33,25 +33,25 @@ int _atoi(char *s)
 					digit = -digit;
 				n = n * 10 + digit;
 				f = 1;
-				if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 						break;
 				f = 0;
 			}
 			i++;
 	}
 
-	if (f == 0)
+			if (f == 0)
 			return (0);
-	
+
 	return (n);
 }
 
 /**
- * main - adds two positive number 
+ * main - adds two positive number
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 (Success), or 1 (Success)  
+ * Return: 0 (Success), or 1 (Success)
 */
 int main(int argc, char *argv[])
 {
@@ -59,14 +59,14 @@ int main(int argc, char *argv[])
 
 	sum = 0;
 
-	for ( i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv [i][j] > '9' || argv[i][j] < '0')
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
 				puts("Error");
-				return (1); 
+				return (1);
 			}
 		}
 	}
