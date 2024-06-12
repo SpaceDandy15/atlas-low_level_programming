f30bb859c50448d10e1ec66a24bfcd52a6b1bd67
Malik Vance <malik@atlassian.com>
2024-06-11 19:29:58 -0500

    quick fix

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: array of arguments
  * Return: Always 0 (Success)
 */
-int main(intargc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
