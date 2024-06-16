a7790ba7659454db884973fa8003300ab2580e86
Malik Vance <malik@atlassian.com>
2024-06-16 11:47:09 -0500

    fix

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,7 @@
  * *malloc_checked - allocates memory using malloc
  * @b: number of bytes to allocate
  *
- * Return a pointer to the allocated memory
+ * Return: a pointer to the allocated memory
 */
 void *malloc_checked(unsigned int b)
 {
