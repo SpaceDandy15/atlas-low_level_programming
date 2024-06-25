1260c4df3bdab3247d5574a1bdbd2ef8ae769e61
Malik Vance <malik@atlassian.com>
2024-06-24 21:41:58 -0500

    fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -15,11 +15,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-		printf("%d,", va_arg(list, int));
+			printf("%d,", va_arg(list, int));
 		else if (separator && i == 0)
-		printf("%d", va_arg(list, int));
+			printf("%d", va_arg(list, int));
 		else
-		printf("%s%d", separator, va_arg(list, int));
+			printf("%s%d", separator, va_arg(list, int));
 	}
 
 	va_end(list);
