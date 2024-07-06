5c3fbe9b57080d697329e3790274d1190b42e881
Malik Vance <malik@atlassian.com>
2024-07-05 20:43:21 -0500

    setup my singly linked list node structure. Updated read me

--- singly_linked_lists/README.md
+++ singly_linked_lists/README.md
@@ -1,1 +1,2 @@
 0-print_list.c is a function that prints all the elements of a list_t list.
+lists.h is the header file that has the prototypes for each function and definition of the list_struct. 
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/**
+ * struct list_s - singly linked list
+ * @len: length of the string
+ * @str: string - (malloc'ed string)
+ * @next: points to the next node
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+size_t prints_list(const list_t *h);
+
+#endif
