14475f75730d8fe46307b3422825c6ce18089adb
Malik Vance <malik@atlassian.com>
2024-07-07 10:28:42 -0500

    fix

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * pop_list - deletes teh head node of a linked list
+ * pop_listint - deletes teh head node of a linked list
  * @head: pointer to the first element in the linked list
  *
  * Return: the data inside the elements that was deleted,
