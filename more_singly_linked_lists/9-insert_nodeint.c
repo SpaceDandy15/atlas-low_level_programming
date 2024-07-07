974bed131bf0f6dd7234461e013c826982426b8d
Malik Vance <malik@atlassian.com>
2024-07-07 11:19:31 -0500

    whitespace on line 3 and space before parenthesis on line 20

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-/** 
+/**
  * insert_nodeint_at_index - inserts a new node in a linked list,
  * at a given position
  * @head: pointer to the first node in the list
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if(!new || !head)
+	if (!new || !head)
 	return (NULL);
 	new->n = n;
 	new->next = NULL;
