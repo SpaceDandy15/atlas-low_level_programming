6d0c252c16294401c9fdd18849cb291b43c555a7
Malik Vance <malik@atlassian.com>
2024-07-07 11:30:50 -0500

    fixed betty issuer of white space on line 8 and space before parenthesis on line 28

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,7 @@
  * @index: index of the node to delete
  * @head: pointer to the first element in the list
  *
- *Return 1(Success), or -1 (Fail) 
+ * Return 1(Success), or -1 (Fail)
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
-		if(!temp || !(temp->next))
+		if (!temp || !(temp->next))
 		return (-1);
 		temp = temp->next;
 		i++;
