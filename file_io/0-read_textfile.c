9a9f0e6ac6056bf47b8d4299ff5cdc110be77ffc
Malik Vance <malik@atlassian.com>
2024-07-08 21:48:24 -0500

    betty really not happy tonight

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,9 +4,11 @@
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
- * @letter: The number of letters the function should read and print.
+ * @letter: The number of letters the 
+ * function should read and print.
  *
  * Return: If the funciton fails or filename is NULL - 0.
+ * O/w - the actual number of bytes the function can read and print.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
