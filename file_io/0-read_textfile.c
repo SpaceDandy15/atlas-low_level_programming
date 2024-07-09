bf9b29e14c2625d2c326ddbf0a3d1090f3bc9961
Malik Vance <malik@atlassian.com>
2024-07-08 21:45:57 -0500

    fixed betty issues line 16 had no space and line 37 had trailing whitespace and main did not end with new line

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o, r, w;
 	char *buffer;
 
-	if(filename == NULL)
+	if (filename == NULL)
 	return (0);
 
 	buffer = malloc(sizeof(char) * letters);
@@ -34,5 +34,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(o);
 
 	return (w);
-	
+
 }
