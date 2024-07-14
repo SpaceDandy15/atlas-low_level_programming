b6ede75a4d68cca40790b67f2c511315e0500d80
Malik Vance <malik@atlassian.com>
2024-07-13 22:09:26 -0500

    size of line 16

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 	dlistint_t *h;
 
-	new = malloc(size(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	return (NULL);
 
