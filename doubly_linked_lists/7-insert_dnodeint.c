7dd1f7e838439107d3d1ab1c2be620565744805d
Malik Vance <malik@atlassian.com>
2024-07-14 11:10:02 -0500

    kernel wanted space in front of parenthesis on line 37

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				else
 				{
 					new = malloc(sizeof(dlistint_t));
-					if(new != NULL)
+					if (new != NULL)
 					{
 						new->n = n;
 						new->next = head->next;
