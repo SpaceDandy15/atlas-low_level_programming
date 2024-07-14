d6160a6e8fc391c6f083be639dc7fb0d6d38e226
Malik Vance <malik@atlassian.com>
2024-07-14 00:18:42 -0500

    fixes

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,13 +6,13 @@
  * @head: head of the list
  * Return: no return
 */
-void free_dlistint(dlisstint_t *head)
+void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
 	if (head != NULL)
 	while (head ->prev != NULL)
-	head = head-prev;
+	head = head->prev;
 
 	while ((tmp = head) != NULL)
 	{
