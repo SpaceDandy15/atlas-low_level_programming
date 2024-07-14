57d5a787eee432cf212b35db3bedda329cd9236e
Malik Vance <malik@atlassian.com>
2024-07-14 18:49:44 -0500

    unsigned line 9

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -6,7 +6,7 @@
  *
  * Return: The calcuated hash
 */
-unsinged long int hash_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 		int c;
