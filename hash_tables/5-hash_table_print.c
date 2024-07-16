2068106d71221d498fdcd050b5c3ce6931aea33c
Malik Vance <malik@atlassian.com>
2024-07-15 21:16:07 -0500

    blank line after declaration line 15 and i++ not i ++ line 19

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -13,10 +13,11 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	unsigned char comma_flag = 0;
 	if (ht == NULL)
+
 	return;
 
 	printf("{");
-	for (i = 0; i < ht->size; i ++)
+	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
