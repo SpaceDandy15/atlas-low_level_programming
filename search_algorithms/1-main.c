73e8dda9410ad067e7d135f0f79a4d87df97d5a9
Malik Vance <malik@atlassian.com>
2024-08-11 17:40:12 -0500

    extra new line

--- search_algorithms/1-main.c
+++ search_algorithms/1-main.c
@@ -18,6 +18,6 @@ int main(void)
 
 	printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
 	printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-	printf("Found %d at index: %d\n\n", 999, binary_search(array, size, 999));
+	printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
 	return (EXIT_SUCCESS);
-	}
+}
